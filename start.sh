ab11531eef558df43c87081315a942997de508b4
penyoofficial <penyoofficial@outlook.com>
2024-10-11 15:23:17 +0800

    test: freeze data

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx1250M -Xms1000M -javaagent:authlib-injector-1.2.5.jar=https://littleskin.cn/api/yggdrasil -jar server.jar --nogui
+java -Xmx6000M -Xms1000M -javaagent:authlib-injector-1.2.5.jar=https://littleskin.cn/api/yggdrasil -jar server.jar --nogui
