48e1c8dd6e5601aa0b00119c7f0de7bc601eb951
Penyo <penyoofficial@outlook.com>
2024-10-15 11:39:30 +0800

    feat: update server to 1.21.2-pre3

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Xmx6000M -Xms1000M -javaagent:authlib-injector-1.2.5.jar=https://littleskin.cn/api/yggdrasil -jar server.jar --nogui
+java -Xms500M -Xmx6G -javaagent:authlib-injector.jar=https://littleskin.cn/api/yggdrasil -jar server.jar --nogui
